525c7bfee4939486dce8eb566b807ce2fcd33177
Adrián Davia <adriandavia@users.noreply.github.com>
2019-03-23 11:46:06 +0100

    Practicas 1-4

---
+++ aplicaciones/primitivas/ApuestaPrimitiva.java
@@ -0,0 +1,82 @@
+package aplicaciones.primitivas;
+
+import librerias.estructurasDeDatos.modelos.ListaConPI;
+import librerias.estructurasDeDatos.lineales.LEGListaConPI;
+import librerias.estructurasDeDatos.lineales.LEGListaConPIOrdenada;
+
+/** 
+ * ApuestaPrimitiva: representa una apuesta aleatoria de La Primitiva, 
+ * o combinacion de 6 numeros distintos entre el 1 y el 49 elegidos  
+ * de forma aleatoria.
+ * 
+ * @version Febrero 2019
+ */
+
+public class ApuestaPrimitiva {
+    
+    // Una Primitiva TIENE UNA Lista con PI que almacena
+    // una combinacion de 6 numeros de La Primitiva
+    private ListaConPI<NumeroPrimitiva> combinacion;
+    
+    /**
+     * Crea una ApuestaPrimitiva, o una combinacion de  
+     * seis numeros aleatorios con valores distintos y   
+     * en el intervalo [1, 49].
+     * 
+     * @param ordenada Un boolean que indica si la combinacion,  
+     *                 sus 6 numeros, debe estar ordenada Asc.
+     *                 (true) o no (false).           
+     */
+    public ApuestaPrimitiva(boolean ordenada) {
+        /* COMPLETAR */
+        if(ordenada) {
+            combinacion = new LEGListaConPIOrdenada();
+        } else {
+            combinacion = new LEGListaConPI();
+        }
+        
+        for(int i = 0; i < 6; i++) {
+                NumeroPrimitiva e = new NumeroPrimitiva();
+                combinacion.insertar(e);
+            }
+    }
+    
+    /**
+     * Devuelve la posicion del numero n en una ApuestaPrimitiva, 
+     * o -1 si n no forma parte de la combinacion. 
+     * IMPORTANTE: se asume que el primer elemento de una combinacion 
+     * esta en su posicion 0 y el ultimo en la 5.
+     * 
+     * @param n un int
+     * @return  la posicion de n en una combinacion, un valor int
+     *          en el intervalo [0, 5] si n esta en la combinacion      
+     *          o -1 en caso contrario
+     */
+    protected int posicionDe(NumeroPrimitiva n) {
+        /* COMPLETAR */
+        int i = 0;
+        while(!combinacion.esFin() && !combinacion.recuperar().equals(n)){
+            combinacion.siguiente();
+            i++;
+        }
+        if (combinacion.esFin()) { return -1;}
+        return i;
+    }
+    
+    /**
+     * Devuelve el String que representa una ApuestaPrimitiva en el formato
+     * texto que muestra el siguiente ejemplo: "16, 25, 28, 49, 9, 20"
+     * 
+     * @return el String con la ApuestaPrimitiva en el formato texto dado. 
+     */
+    public String toString() {
+        /* COMPLETAR */
+        String msj = "";
+        combinacion.inicio();
+        while(!combinacion.esFin()) {
+            msj += combinacion.recuperar().toString() + ", ";
+            combinacion.siguiente();
+        }
+        return msj;
+    }
+}
